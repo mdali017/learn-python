7d19c5e7e85b188c4be31224241288fe66e38996
mdali017 <mohabbatalit8@gmail.com>
2024-03-22 15:00:41 +0600

    Big picture of Data Type

---
+++ 06_DataTypes/dataType.py
@@ -0,0 +1,55 @@
+
+# ************** Number Data Type
+# Type of Data Type
+# 1. Number = 1234, 3.1416, j+4j, ob111, Decimal(
+
+mylist = [123, "chai", 3.14]
+len(mylist)
+# print("hello")
+
+
+# this is comment
+
+# print(12 + 12);
+# print(2.5 * 5);
+# print(2 **  4);
+# print(2 ** 100)
+
+# math method
+import math;
+# print(math.pi);
+
+# random
+import random;
+# print(random.random());
+# print(random.choice([1, 2, 3, 4, 5]));
+
+
+# *********** String Data Type ************
+username = "chaiaurcode";
+# print(len(username))
+# print(username[0]);
+# username[0] = "A";
+# print(username[-1])
+# print(username[-2]);
+# print(username[1:3]);
+# print(dir(username));
+
+# *********** List Data Type ************
+myList = [123, "chai", 3.1416]
+# print(mylist);
+# print(len(mylist))
+
+
+# *********** Dictionary Data Type ************
+myDictionnary = {"one": "lemon", "second": "ginger", "comic": "nagraj"};
+# print(myDictionnary)
+# print(myDictionnary["comic"])
+# print(myDictionnary["one"])
+
+# *********** Tuple Data Type ************
+myTuple = (1, 2, 3);
+print(myTuple)
+print(myTuple[0]);
+print(len(myTuple));
+

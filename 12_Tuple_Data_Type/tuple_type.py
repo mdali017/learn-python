d5f79cf3911de0192fe28763da21a5c354b7cf8c
mdali017 <mohabbatalit8@gmail.com>
2024-03-24 00:02:09 +0600

    tuple Data Type leran

---
+++ 12_Tuple_Data_Type/tuple_type.py
@@ -0,0 +1,13 @@
+
+
+# ********* Welcome To Tuple Data Type ************
+# print("Welcome To Tuple Data Type")
+
+# tuple data type immutable 
+tea_types =("Black", "Green", "Oolong");
+print(tea_types)
+print(tea_types[0])
+print(tea_types[-1])
+print(tea_types[1:])
+
+print(len(tea_types))
